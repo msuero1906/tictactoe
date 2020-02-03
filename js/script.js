270046baa00350e727b653a183ab927bcf3dd39f
msuero1906 <msuero1906.student@hsfi.us>
2020-02-03 03:36:08 +0000

    I'm finally done.

--- js/script.js
+++ js/script.js
@@ -2,22 +2,58 @@ var score = "x";
 var turns =  0;
 var gameEnd = false;
 
-function performLogic(buttonId,tileId){
-    $(buttonId).html(score); 
-     if (score === "x") {
-         score = "o";
-     }
-
-     else if(score ==="o"){
-         score ="x";
-     }
-
-
+function change_Players(){
+    turns = turns + 1;
+    if(score === "x"){
+        score = "o";
+    } 
+    else if(score === "o"){
+        score = "x";
+    }
 }
 
-function draw(){
-    $(h1).html("It's a draw!");
+
+
+function game_tie() {
+     if(turns > 8 && gameEnd === false){
+        $("h1").html("It's a draw!");
+        gameEnd = true;
+     }
 }
+
+function pw(tileId1, tileId2, tileId3){
+    if ($(tileId1).html() === score && $(tileId2).html() === score && $(tileId3).html() === score){
+        $("h1").text(score + " wins!");
+        gameEnd = true;
+    }
+}
+function VerticalWins(){
+    pw("#tile1", "#tile4", "#tile7");
+    pw("#tile2", "#tile5", "#tile8");
+    pw("#tile3", "#tile6", "#tile9");
+}
+function HorizontalWins(){
+    pw("#tile1", "#tile2", "#tile3");
+    pw("#tile4", "#tile5", "#tile6");
+    pw("#tile7", "#tile8", "#tile9");
+}
+function DiagonalWins(){
+    pw("#tile1", "#tile5", "#tile9");
+    pw("#tile3", "#tile5", "#tile7");
+}
+ 
+function performLogic(button, tile){
+    if(gameEnd === false){
+        $(button).hide();
+        $(tile).html(score);
+        DiagonalWins();
+        HorizontalWins();
+        VerticalWins();
+        change_Players();
+        game_tie();
+    }
+}
+
 
 
 
